359c8181b02484005a1e8a4cadf5fe1857ead749
KANekT <kanektikys@gmail.com>
2013-02-16 19:18:29 +0400

    Delete Articles
    This plugon is not support

--- articles/views/frontend/last.view.php
+++
@@ -1,3 +0,0 @@
-<?php foreach($records as $row):?>
-    <li><a href="<?php echo $site_url;?>articles/<?php echo $row['id'];?>/<?php echo $row['slug'];?>"><?php echo $row['name'];?></a></li>              
-<?php endforeach;?>
